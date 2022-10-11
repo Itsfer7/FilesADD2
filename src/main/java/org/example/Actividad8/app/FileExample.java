281b1915568d7e59eb1bde1d5fae09b3367c4111
Ivan <ivanfergarcia@gmail.com>
2022-10-11 09:21:20 +0200

    Actualización 11/10/22

--- src/main/java/org/example/Actividad8/app/FileExample.java
+++ src/main/java/org/example/Actividad8/app/FileExample.java
@@ -6,8 +6,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileExample {
-    private static final String NOMBRE_FICHERO_RESULTADO_FICHEROS = "/home/ivan/Actividad8_FicheroResultado_Ficheros.txt";
     private static final String NOMBRE_FICHERO_RESULTADO_DIRECTORIOS = "/home/ivan/Actividad8_FicheroResultado_Directorios.txt";
+    private static final String NOMBRE_FICHERO_RESULTADO_FICHEROS = "/home/ivan/Actividad8_FicheroResultado_Ficheros.txt";
 
 
     public static void main(String[] args) throws IOException {
